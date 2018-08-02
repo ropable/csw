ed5031eb2b125db7b50169d0e006510844a0a31f
Ashley Felton <ashley@ropable.com>
2018-08-02 15:07:00 +0800

    Set default URL path.

--- csw/urls.py
+++ csw/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogue/', include('catalogue.urls')),
+    path('', RedirectView.as_view(url='/admin/')),
 ]
