1b5994a717115689408d7825bdb664e4951753a4
Ashley Felton <ashley@ropable.com>
2018-08-02 13:15:36 +0800

    Update requirements lib versions.

--- csw/wsgi.py
+++ csw/wsgi.py
@@ -1,17 +1,16 @@
 """
 WSGI config for csw project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csw.settings")
-
 import confy
-confy.read_environment_file(".env")
-
 from django.core.wsgi import get_wsgi_application
+import os
+from pathlib import Path
+
+d = Path(__file__).resolve().parents[1]
+dot_env = os.path.join(str(d), '.env')
+if os.path.exists(dot_env):
+    confy.read_environment_file(dot_env)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csw.settings")
 application = get_wsgi_application()
